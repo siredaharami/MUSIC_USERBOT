30b5c61dfe57addb002105b69977ce0e50b3bc29
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-05-24 14:00:21 +0530

    Add files via upload

---
+++ PBXMUSIC/__init__.py
@@ -0,0 +1,29 @@
+from PBXMUSIC.core.bot import PBX
+from PBXMUSIC.core.dir import dirr
+from PBXMUSIC.core.git import git
+from PBXMUSIC.core.userbot import Userbot
+from PBXMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = PBX()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+APP = "tg_vc_bot"  # connect music api key "Dont change it"
---
+++ PBXMUSIC/__main__.py
@@ -0,0 +1,64 @@
+import asyncio
+import importlib
+from sys import argv
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from PBXMUSIC import LOGGER, app, userbot
+from PBXMUSIC.core.call import PBX
+from PBXMUSIC.misc import sudo
+from PBXMUSIC.plugins import ALL_MODULES
+from PBXMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+from PBXMUSIC.plugins.tools.clone import restart_bots
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error(
+            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬"
+        )
+
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            if user_id not in BANNED_USERS:
+                BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("PBXMUSIC.plugins" + all_module)
+    LOGGER("PBXMUSIC.plugins").info("ꜱᴏ ꜱᴀᴅ 🥳...")
+
+    await userbot.start()
+
+    await PBX.start()
+    await PBX.decorators()
+    await restart_bots()
+    LOGGER("PBXMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️ᴍᴀᴅᴇ ʙʏ ᴛᴇᴀᴍ ᴘʙx ♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    await idle()
+
+    await app.stop()
+    await userbot.stop()
+
+    LOGGER("PBXMUSIC").info(
+        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️ᴍᴀᴅᴇ ʙʏ ᴛᴇᴀᴍ ᴘʙx ♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+
+
+if __name__ == "__main__":
+
+    asyncio.get_event_loop().run_until_complete(init())
