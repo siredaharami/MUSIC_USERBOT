1757a8bb0dbfcca1b30541490b59e9a7e9fb71dd
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-05-24 23:00:32 +0530

    Update live.py

--- PBXMUSIC/plugins/play/live.py
+++ PBXMUSIC/plugins/play/live.py
@@ -1,60 +1,81 @@
 from pyrogram import filters
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
+from pyrogram.types import Message
 
-from PBXMUSIC import YouTube, app
-from PBXMUSIC.utils.channelplay import get_channeplayCB
-from PBXMUSIC.utils.decorators.language import languageCB
-from PBXMUSIC.utils.stream.stream import stream
+from PBXMUSIC import app
+from PBXMUSIC.utils.database import set_cmode
+from PBXMUSIC.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
+from time import time
+import asyncio
+from PBXMUSIC.utils.extraction import extract_user
+
+# Define a dictionary to track the last message timestamp for each user
+user_last_message_time = {}
+user_command_count = {}
+# Define the threshold for command spamming (e.g., 20 commands within 60 seconds)
+SPAM_THRESHOLD = 2
+SPAM_WINDOW_SECONDS = 5
 
 
-@app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
-@languageCB
-async def play_live_stream(client, CallbackQuery, _):
-    callback_data = CallbackQuery.data.strip()
-    callback_request = callback_data.split(None, 1)[1]
-    vidid, user_id, mode, cplay, fplay = callback_request.split("|")
-    if CallbackQuery.from_user.id != int(user_id):
-        try:
-            return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
-        except:
-            return
-    try:
-        chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
-    except:
-        return
-    video = True if mode == "v" else None
-    user_name = CallbackQuery.from_user.first_name
-    await CallbackQuery.message.delete()
-    try:
-        await CallbackQuery.answer()
-    except:
-        pass
-    mystic = await CallbackQuery.message.reply_text(
-        _["play_2"].format(channel) if channel else _["play_1"]
-    )
-    try:
-        details, track_id = await YouTube.track(vidid, True)
-    except:
-        return await mystic.edit_text(_["play_3"])
-    ffplay = True if fplay == "f" else None
-    if not details["duration_min"]:
-        try:
-            await stream(
-                _,
-                mystic,
-                user_id,
-                details,
-                chat_id,
-                user_name,
-                CallbackQuery.message.chat.id,
-                video,
-                streamtype="live",
-                forceplay=ffplay,
+@app.on_message(filters.command(["channelplay"]) & filters.group & ~BANNED_USERS)
+@AdminActual
+async def playmode_(client, message: Message, _):
+    user_id = message.from_user.id
+    current_time = time()
+    # Update the last message timestamp for the user
+    last_message_time = user_last_message_time.get(user_id, 0)
+
+    if current_time - last_message_time < SPAM_WINDOW_SECONDS:
+        # If less than the spam window time has passed since the last message
+        user_last_message_time[user_id] = current_time
+        user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
+        if user_command_count[user_id] > SPAM_THRESHOLD:
+            # Block the user if they exceed the threshold
+            hu = await message.reply_text(
+                f"**{message.from_user.mention} ᴘʟᴇᴀsᴇ ᴅᴏɴᴛ ᴅᴏ sᴘᴀᴍ, ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 5 sᴇᴄ**"
             )
-        except Exception as e:
-            ex_type = type(e).__name__
-            err = e if ex_type == "AssistantErr" else _["general_2"].format(ex_type)
-            return await mystic.edit_text(err)
+            await asyncio.sleep(3)
+            await hu.delete()
+            return
     else:
-        return await mystic.edit_text("» ɴᴏᴛ ᴀ ʟɪᴠᴇ sᴛʀᴇᴀᴍ.")
-    await mystic.delete()
+        # If more than the spam window time has passed, reset the command count and update the message timestamp
+        user_command_count[user_id] = 1
+        user_last_message_time[user_id] = current_time
+
+    if len(message.command) < 2:
+        return await message.reply_text(_["cplay_1"].format(message.chat.title))
+    query = message.text.split(None, 2)[1].lower().strip()
+    if (str(query)).lower() == "disable":
+        await set_cmode(message.chat.id, None)
+        return await message.reply_text(_["cplay_7"])
+    elif str(query) == "linked":
+        chat = await app.get_chat(message.chat.id)
+        if chat.linked_chat:
+            chat_id = chat.linked_chat.id
+            await set_cmode(message.chat.id, chat_id)
+            return await message.reply_text(
+                _["cplay_3"].format(chat.linked_chat.title, chat.linked_chat.id)
+            )
+        else:
+            return await message.reply_text(_["cplay_2"])
+    else:
+        try:
+            chat = await app.get_chat(query)
+        except:
+            return await message.reply_text(_["cplay_4"])
+        if chat.type != ChatType.CHANNEL:
+            return await message.reply_text(_["cplay_5"])
+        try:
+            async for user in app.get_chat_members(
+                chat.id, filter=ChatMembersFilter.ADMINISTRATORS
+            ):
+                if user.status == ChatMemberStatus.OWNER:
+                    cusn = user.user.username
+                    crid = user.user.id
+        except:
+            return await message.reply_text(_["cplay_4"])
+        if crid != message.from_user.id:
+            return await message.reply_text(_["cplay_6"].format(chat.title, cusn))
+        await set_cmode(message.chat.id, chat.id)
+        return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))
